ed57b5fd9c74bd2500d5ea73e4e3dcb3302b7234
Hallcoder <hallcoder25@gmail.com>
2022-07-31 11:27:06 +0200

    state management with TypeScript and React

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-import { useState } from 'react'
-import NewTodo from './components/NewTodo';
-import TodoList from './components/Todolist'
-
-const App: React.FC= ()=>{
-  const todos = [{ id: "t1", text: "Finish the course" }];
-  return (
+import { useState } from "react";
+import NewTodo from "./components/NewTodo";
+import TodoList from "./components/Todolist";
+import { Todo } from "./todo.model";
+const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]|{ id:string,text:string}[]>([]);
+  const todoAddHandler = (text: string) => {
+  setTodos((prevTodos) => [{id:Math.random().toString(),text:text},...prevTodos]);  
+  }
+    return (
     <div className="App">
-      <NewTodo />
-     <TodoList items={todos}/>
+      <NewTodo handler={todoAddHandler} />
+      <TodoList items={todos} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
